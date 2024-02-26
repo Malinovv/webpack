8017e015973b69201bdde93f018cc90f44ff466f
Kristijan Malinov <kristijan.malinovv@gmail.com>
2024-02-26 15:36:11 +0100

    learning code compiling and development server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from "lodash";
 import myName from "./myName";
+import "./style.css";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
-
+  const btn = document.createElement("button");
   // Lodash, now imported by this script
   element.textContent = myName("Kristijan");
+  element.classList.add("hello");
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
